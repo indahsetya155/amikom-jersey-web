e720b02513ac95a3a43665d864e6ce4a7b319016
Arif's Macbook <arifpujinugroho@gmail.com>
2022-01-24 11:06:02 +0700

    adding tambah dan kurang orderan

---
+++ aksi_kurang_order.php
@@ -0,0 +1,9 @@
+<?php
+session_start();
+$sid = session_id();
+include "lib/koneksi.php";
+$id_pro = $_GET['id_order'];
+
+// untuk tambah  
+mysqli_query($koneksi, "UPDATE tbl_order SET jumlah = jumlah-1 WHERE id_order='$id_pro'");
+header('location:keranjang.php');
--- pages/cart.php
+++ pages/cart.php
@@ -1,50 +1,53 @@
-
-	<?php
+<?php
 $sid = session_id();
-	?>
+?>
 
-	<section id="cart_items">
-		<div class="container">
-			<div class="breadcrumbs">
-				<ol class="breadcrumb">
-				  <li><a href="#">Home</a></li>
-				  <li class="active">Shopping Cart</li>
-				</ol>
-			</div>
-			<div class="table-responsive cart_info">
-				<table class="table table-condensed">
-					<thead>
-						<tr class="cart_menu">
-							<td class="image">Gambar</td>
-							<td class="description">Nama Produk</td>
-							<td class="price">Harga</td>
-							<td class="quantity">Jumlah</td>
-							<td class="total">Sub Total</td>
-							<td></td>
-						</tr>
-					</thead>
-					<tbody>
-						<?php
-						$sql = mysqli_query($koneksi, "SELECT * FROM tbl_order, tbl_produk WHERE tbl_order.id_session='$sid' AND tbl_order.id_produk=tbl_produk.id_produk");
+<section id="cart_items">
+	<div class="container">
+		<div class="breadcrumbs">
+			<ol class="breadcrumb">
+				<li><a href="#">Home</a></li>
+				<li class="active">Shopping Cart</li>
+			</ol>
+		</div>
+		<div class="table-responsive cart_info">
+			<table class="table table-condensed">
+				<thead>
+					<tr class="cart_menu text-center">
+						<td class="image">Gambar</td>
+						<td class="description">Nama Produk</td>
+						<td class="price">Harga</td>
+						<td class="quantity">Jumlah</td>
+						<td class="total">Sub Total</td>
+						<td></td>
+					</tr>
+				</thead>
+				<tbody>
+					<?php
+					$sql = mysqli_query($koneksi, "SELECT * FROM tbl_order, tbl_produk WHERE tbl_order.id_session='$sid' AND tbl_order.id_produk=tbl_produk.id_produk");
 
-						while($r=mysqli_fetch_array($sql)){
-							$subtotal = $r['harga']*$r['jumlah'];
+					while ($r = mysqli_fetch_array($sql)) {
+						$subtotal = $r['harga'] * $r['jumlah'];
 
-						
-						?>
+
+					?>
 						<tr>
-							<td class="cart_product">
-								<a href=""><img src="admin/upload/<?php echo $r['gambar'];?>" alt=""></a>
+							<td class="cart_product text-center">
+								<a href=""><img src="admin/upload/<?php echo $r['gambar']; ?>" style="height: 120px;" class="rounded img-thumbnail" alt=""></a>
 							</td>
 							<td class="cart_description">
-								<h4><a href=""><?php echo $r['nama_produk'];?> </a></h4>
-								
+								<h4><a href=""><?php echo $r['nama_produk']; ?> </a></h4>
+
 							</td>
-							<td class="cart_price">
-								<p>Rp. <?php echo $r['harga'];?></p>
+							<td class="cart_price text-right">
+								<p>Rp. <?php echo $r['harga']; ?></p>
 							</td>
-							<td class="cart_quantity">
-								<?php echo $r['jumlah'];?>
+							<td class="cart_quantity text-center">
+								<?php if ($r['jumlah'] > 1) {
+									echo '<a class="cart_quantity_delete" href="aksi_kurang_order.php?id_order=' . $r['id_order'] . '"><i class="fa fa-minus-circle"></i></a>';
+								} ?>
+								<?php echo $r['jumlah']; ?>
+								<a class="cart_quantity_delete" href="aksi_tambah_order.php?id_order=<?php echo $r['id_order']; ?>"><i class="fa fa-plus-circle"></i></a>
 							</td>
 							<td class="cart_total">
 								<p class="cart_total_price">Rp. <?php echo number_format($subtotal); ?></p>
@@ -53,11 +56,12 @@ $sid = session_id();
 								<a class="cart_quantity_delete" href="aksi_hapus_keranjang.php?id_order=<?php echo $r['id_order']; ?>"><i class="fa fa-times"></i></a>
 							</td>
 						</tr>
-							<?php } ?>
-						
-					</tbody>
-				</table>
-			</div>s
-			<a href="selesai.php" class="btn btn-default add-to-cart">Checkout</a>
+					<?php } ?>
+
+				</tbody>
+			</table>
 		</div>
-	</section> <!--/#cart_items-->
+		<a href="selesai.php" class="btn btn-default add-to-cart">Checkout</a>
+	</div>
+</section>
+<!--/#cart_items-->
